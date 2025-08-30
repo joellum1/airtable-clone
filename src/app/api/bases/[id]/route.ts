48e830a197b8a82adbb53c22bd5fe8ea127e7f57
Joel Lum <joellumzh@gmail.com>
2025-08-31 01:48:09 +1000

    added interface for type safety when modifying bases + updated code style

--- src/app/api/bases/[id]/route.ts
+++ src/app/api/bases/[id]/route.ts
@@ -2,31 +2,42 @@ import { prisma } from "@/lib/prisma";
 
 // route - api/bases/:id
 
+interface PatchRequestBody {
+    name?: string;
+}
+
 // retrieve base by id
 export const GET = async (req: Request, { params }: { params: Promise<{ id: string }> }) => {
     const { id } = await params;
+
     const base = await prisma.base.findUnique({
         where: { id }
     });
+
     return Response.json(base);
 };
 
 // update base information
 export const PATCH = async (req: Request, { params }: { params: Promise<{ id: string }> }) => {
     const { id } = await params;
-    const { name } = await req.json();
+    const body = await req.json() as PatchRequestBody;
+    const { name } = body;
+
     const updatedBase = await prisma.base.update({
         where: { id },
         data: { name }
     });
+
     return Response.json(updatedBase);
 };
 
 // delete base by id
 export const DELETE = async (req: Request, { params }: { params: Promise<{ id: string }> }) => {
     const { id } = await params;
+
     const res = await prisma.base.delete({
         where: { id }
     });
+
     return Response.json(res);
 };
