722c4b9f02d67450f4aa2b8ae625d6165ec9cf23
Joel Lum <joellumzh@gmail.com>
2025-08-31 01:36:06 +1000

    fixed global prisma variable declaration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-    interface globalThis {
-        prisma?: PrismaClient;
-    }
+    var prisma: PrismaClient | undefined;
 }
 
 // create new prisma client if doesn't already exist
-export const prisma = globalThis.prisma || new PrismaClient();
+export const prisma = globalThis.prisma ?? new PrismaClient();
