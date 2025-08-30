ebc2747db58beb1f998186dab0e1f3dc7b2764a7
Joel Lum <joellumzh@gmail.com>
2025-08-31 01:47:15 +1000

    removed unnecessary req arguments

--- src/app/api/bases/route.ts
+++ src/app/api/bases/route.ts
@@ -3,13 +3,13 @@ import { prisma } from "@/lib/prisma";
 // route - api/bases
 
 // list all bases
-export const GET = async (req: Request) => {
+export const GET = async () => {
     const bases = await prisma.base.findMany();
     return Response.json(bases);
 };
 
 // create new base with default configurations
-export const POST = async (req: Request) => {
+export const POST = async () => {
     const newBase = await prisma.base.create({
         data: {}
     });
