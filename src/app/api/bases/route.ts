27e8a760330d32578c63407ffec9bc8aebe6c8da
Joel Lum <joellumzh@gmail.com>
2025-08-31 01:27:42 +1000

    added api endpoints for base creation, modification, and deletion

---
+++ src/app/api/bases/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/lib/prisma";
+
+// route - api/bases
+
+// list all bases
+export const GET = async (req: Request) => {
+    const bases = await prisma.base.findMany();
+    return Response.json(bases);
+};
+
+// create new base with default configurations
+export const POST = async (req: Request) => {
+    const newBase = await prisma.base.create({
+        data: {}
+    });
+    return Response.json(newBase);
+};
