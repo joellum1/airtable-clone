53bbcc1c8dd22980623757e9dd3757b2d85779d0
Joel Lum <joellumzh@gmail.com>
2025-08-31 16:42:43 +1000

    added API endpoint for retrieval and creation of fields

---
+++ src/app/api/field/route.ts
@@ -0,0 +1,57 @@
+import { prisma, FieldType } from "@/lib/prisma";
+
+// route - api/field?tableId=<tableId>
+
+interface PostRequestBody {
+    tableId: string,
+    name?: string,
+    type: FieldType
+} 
+
+// list all fields in a table
+export const GET = async (req: Request) => {
+    const { searchParams } = new URL(req.url);
+    const tableId = searchParams.get("tableId");
+
+    if (!tableId || typeof tableId !== "string") {
+        return Response.json(
+            { error: "tableId variable needs to be a non-empty string" },
+            { status: 400 }
+        );
+    }
+    
+    const fields = await prisma.field.findMany({
+        where: { tableId }
+    });
+
+    return Response.json(fields);
+};
+
+// create new field in a table
+export const POST = async (req: Request) => {
+    const body = await req.json() as PostRequestBody;
+    const { tableId, name, type } = body;
+
+    if (!tableId || typeof tableId !== "string") {
+        return Response.json(
+            { error: "tableId variable needs to be a non-empty string" },
+            { status: 400 }
+        );
+    }
+
+    const position = await prisma.field.count({
+        where: { tableId },
+    });
+
+    const newField = await prisma.field.create({
+        data: { 
+            tableId, 
+            name: name ?? undefined, 
+            type, 
+            position
+        }
+    });
+
+    return Response.json(newField);
+};
+
