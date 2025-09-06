786dc99727621e1853114092fdd4b9b324a31402
Joel Lum <joellumzh@gmail.com>
2025-09-06 21:35:11 +1000

    fixed sizing issue with borders, added all components to minimised sidebar

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -3,43 +3,45 @@ import { Avatar } from "antd";
 
 export default function Header() {
   return (
-    <div className="w-full h-[56px] pl-[8px] pr-[16px] flex justify-between items-center border border-gray-200 shadow-xs z-10">
-      {/* Left Side */}
-      <div className='flex items-center'>
-        {/* Expand Sidebar Icon */}
-        <div className="pl-[4px] pr-[8px] hover:cursor-pointer">
-          <MenuOutlined />
+    <header className="w-full h-[57px] flex items-center flex-none border-b border-gray-200 box-border shadow-xs z-10">
+      <nav className='w-full pl-[8px] pr-[16px] flex justify-between items-center'>
+        {/* Left Side */}
+        <div className='flex items-center'>
+          {/* Expand Sidebar Icon */}
+          <div className="pl-[4px] pr-[8px] hover:cursor-pointer">
+            <MenuOutlined />
+          </div>
+
+          {/* Airtable Logo */}
+          <div className='p-[12px] mr-[4px]'>
+            <img src="/images/airtable_logo_with_name.png" alt="airtable logo" className='w-[102px]'/>
+          </div>
         </div>
 
-        {/* Airtable Logo */}
-        <div className='p-[12px] mr-[4px]'>
-          <img src="/images/airtable_logo_with_name.png" alt="airtable logo" className='w-[102px]'/>
+        {/* Middle - Search Bar */}
+
+        {/* Right Side - List of Miscellaneous Buttons */}
+        <div className='flex items-center'>
+          {/* Support Button */}
+          <button className='w-[28px] h-[28px] flex items-center justify-center rounded-full hover:cursor-pointer hover:bg-gray-200'>
+            <QuestionCircleOutlined style={{ fontSize: '13px' }} />
+          </button>
+
+          {/* Notifications Button */}
+          <button className='w-[28px] h-[28px] mx-[12px] flex items-center justify-center rounded-full border border-solid border-gray-200 shadow-xs hover:cursor-pointer hover:bg-gray-200'>
+            <BellOutlined style={{ fontSize: '14px' }} />
+          </button>
+
+          {/* User Button */}
+          <button className='w-[28px] h-[28px] ml-[8px] flex items-center justify-center rounded-full border border-solid border-gray-200 shadow-xs hover:cursor-pointer hover:shadow-xs'>
+            <Avatar 
+              // src=''
+              icon={<UserOutlined />}
+              size={25}
+            />
+          </button>
         </div>
-      </div>
-
-      {/* Middle - Search Bar */}
-
-      {/* Right Side - List of Miscellaneous Buttons */}
-      <div className='flex items-center'>
-        {/* Support Button */}
-        <button className='w-[28px] h-[28px] flex items-center justify-center rounded-full hover:cursor-pointer hover:bg-gray-200'>
-          <QuestionCircleOutlined style={{ fontSize: '13px' }}/>
-        </button>
-
-        {/* Notifications Button */}
-        <button className='w-[28px] h-[28px] mx-[12px] flex items-center justify-center rounded-full border border-solid border-gray-200 shadow-xs hover:cursor-pointer hover:bg-gray-200'>
-          <BellOutlined style={{ fontSize: '14px' }}/>
-        </button>
-
-        {/* User Button */}
-        <button className='w-[28px] h-[28px] ml-[8px] flex items-center justify-center rounded-full border border-solid border-gray-200 shadow-xs hover:cursor-pointer hover:shadow-xs'>
-          <Avatar 
-            // src=''
-            icon={<UserOutlined />}
-            size={25}
-          />
-        </button>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 }
--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,30 +1,64 @@
-import { HomeOutlined, StarOutlined, ShareAltOutlined, TeamOutlined } from '@ant-design/icons';
+import { HomeOutlined, StarOutlined, ShareAltOutlined, TeamOutlined, PlusOutlined } from '@ant-design/icons';
+import ImportContactsOutlinedIcon from '@mui/icons-material/ImportContactsOutlined';
+import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 
 export default function Sidebar() {
   return (
-    <div className="w-[46px] pt-[20px] flex flex-col items-center border border-gray-200 shadow-xs z-5">
-      {/* Home */}
-      <div className="w-[20px] h-[20px] mb-[20px]">
-        <HomeOutlined />
-      </div>
+    <div className="w-[46px] flex flex-none border-r border-gray-200 box-border shadow-xs z-5">
+      <nav className="pt-[20px] flex flex-col justify-between items-center">
+        {/* Top */}
+        <div className="w-[46px] flex flex-col items-center">
+          {/* Home */}
+          <div className="w-[20px] h-[20px] mb-[20px]">
+            <HomeOutlined />
+          </div>
 
-      {/* Starred */}
-      <div className="w-[20px] h-[20px] mb-[20px]">
-        <StarOutlined />
-      </div>
+          {/* Starred */}
+          <div className="w-[20px] h-[20px] mb-[20px]">
+            <StarOutlined />
+          </div>
 
-      {/* Shared */}
-      <div className="w-[20px] h-[20px] mb-[20px]">
-        <ShareAltOutlined />
-      </div>
+          {/* Shared */}
+          <div className="w-[20px] h-[20px] mb-[20px]">
+            <ShareAltOutlined />
+          </div>
 
-      {/* Workspaces */}
-      <div className="w-[20px] h-[20px] mb-[20px]">
-        <TeamOutlined />
-      </div>
+          {/* Workspaces */}
+          <div className="w-[20px] h-[20px] mb-[20px]">
+            <TeamOutlined />
+          </div>
 
-      {/* Divider */}
-      <div className='w-[20px] h-[1px] border-t border-[rgba(29, 31, 37, 0.3)] mb-[20px]'/>
+          {/* Divider */}
+          <div className='w-[20px] h-[1px] mb-[20px] border-t border-[rgba(29, 31, 37, 0.3)] box-border'/>
+        </div>
+
+        {/* Bottom */}
+        <div className='flex flex-col items-center'>
+          {/* Divider */}
+          <div className='w-[20px] h-[1px] mb-[18px] border-t border-[rgba(29, 31, 37, 0.3)] box-border'/>
+
+          {/* Templates */}
+          <div className='w-[16px] h-[16px] mb-[18px]'>
+            <ImportContactsOutlinedIcon fontSize='inherit' style={{ color: '#8f8f8f' }} />
+          </div>
+
+          {/* Marketplace */}
+          <div className='w-[16px] h-[16px] mb-[18px]'>
+            <WorkOutlineOutlinedIcon fontSize='inherit' style={{ color: '#8f8f8f' }} />
+          </div>
+
+          {/* Import */}
+          <div className='w-[16px] h-[16px] mb-[18px]'>
+            <LanguageOutlinedIcon fontSize='inherit' style={{ color: '#8f8f8f' }} />
+          </div>
+
+          {/* Create */}
+          <div className='w-[22px] h-[22px] flex items-center justify-center border border-[#e4e4e4] box-border rounded-md'>
+            <PlusOutlined style={{ fontSize: '16px', color: '#8f8f8f' }} />
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
